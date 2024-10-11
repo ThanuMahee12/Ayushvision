2c9808eaac5407f31a0229bcf500b93352452c2e
ThanuMahee12 <thanumahee440@gmail.com>
2024-10-11 05:37:06 +0530

    menuCreated

---
+++ app/adminpanel/layout/Menu.py
@@ -0,0 +1,10 @@
+from PyQt5.QtWidgets import QMainWindow, QAction, QMenuBar, QWidget, QVBoxLayout
+
+        
+    
+class MenuBar(QMenuBar):
+    def __init__(self,menus=[],parent=None):
+        super().__init__(parent)
+    def add_menu(self,):
+        self.addMenu()
+        
